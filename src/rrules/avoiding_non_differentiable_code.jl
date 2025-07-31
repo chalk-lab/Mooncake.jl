3cb7976b29c3923645ff62f6e4317113f7c24fbe
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-07-31 20:28:26 +0530

    test_cases-1

--- src/rrules/avoiding_non_differentiable_code.jl
+++ src/rrules/avoiding_non_differentiable_code.jl
@@ -126,50 +126,64 @@ function generate_hand_written_rrule!!_test_cases(
     test_cases = vcat(
         Any[
             # Rules to avoid pointer type conversions.
-            # (
-            #     true,
-            #     :stability_and_allocs,
-            #     nothing,
-            #     +,
-            #     CoDual(
-            #         bitcast(Ptr{Float64}, pointer_from_objref(_x)),
-            #         bitcast(Ptr{Float64}, pointer_from_objref(_dx)),
-            #     ),
-            #     2,
-            # ),
+            (
+                true,
+                :stability_and_allocs,
+                nothing,
+                +,
+                CoDual(
+                    bitcast(Ptr{Float64}, pointer_from_objref(_x)),
+                    bitcast(Ptr{Float64}, pointer_from_objref(_dx)),
+                ),
+                2,
+            ),
 
             # Rules for handling Atomic read operations.
-            (false, :stability_and_allocs, nothing, Base.getindex, Atomic{Int64}(rand(1:100))),
-            # (false, :allocs, nothing, getindex, Atomic{Int32}(rand(1:100))),
-            # (false, :allocs, nothing, getindex, Atomic{Int16}(rand(1:100))),
+            (
+                false,
+                :stability_and_allocs,
+                nothing,
+                Base.getindex,
+                Atomic{Int64}(rand(1:100)),
+            ),
+            (false, :stability_and_allocs, nothing, getindex, Atomic{Int32}(rand(1:100))),
+            (false, :stability_and_allocs, nothing, getindex, Atomic{Int16}(rand(1:100))),
         ],
 
         # Rules in order to avoid introducing determinism.
-        # reduce(
-        #     vcat,
-        #     map([Xoshiro(1), TaskLocalRNG()]) do rng
-        #         return Any[
-        #             (true, :stability_and_allocs, nothing, randn, rng),
-        #             (true, :stability, nothing, randn, rng, 2),
-        #             (true, :stability, nothing, randn, rng, 3, 2),
-        #         ]
-        #     end,
-        # ),
+        reduce(
+            vcat,
+            map([Xoshiro(1), TaskLocalRNG()]) do rng
+                return Any[
+                    (true, :stability_and_allocs, nothing, randn, rng),
+                    (true, :stability, nothing, randn, rng, 2),
+                    (true, :stability, nothing, randn, rng, 3, 2),
+                ]
+            end,
+        ),
 
         # Rules to make string-related functionality work properly.
-        # (false, :stability, nothing, string, 'H'),
+        (false, :stability, nothing, string, 'H'),
         (false, :stability, nothing, Base.normpath, "/home/user/../folder/./file.txt"),
-        (false, :stability, nothing, Base._replace_init, "hello world", ("hello" => "hi",), 1),
+        (
+            false,
+            :stability,
+            nothing,
+            Base._replace_init,
+            "hello world",
+            ("hello" => "hi",),
+            1,
+        ),
 
         # Rules to make Symbol-related functionality work properly.
-        # (false, :stability_and_allocs, nothing, Symbol, "hello"),
-        # (false, :stability_and_allocs, nothing, Symbol, UInt8[1, 2]),
-        # (false, :stability_and_allocs, nothing, Float64, π, RoundDown),
-        # (false, :stability_and_allocs, nothing, Float64, π, RoundUp),
-        # (true, :stability_and_allocs, nothing, Float32, π, RoundDown),
-        # (true, :stability_and_allocs, nothing, Float32, π, RoundUp),
-        # (true, :stability_and_allocs, nothing, Float16, π, RoundDown),
-        # (true, :stability_and_allocs, nothing, Float16, π, RoundUp),
+        (false, :stability_and_allocs, nothing, Symbol, "hello"),
+        (false, :stability_and_allocs, nothing, Symbol, UInt8[1, 2]),
+        (false, :stability_and_allocs, nothing, Float64, π, RoundDown),
+        (false, :stability_and_allocs, nothing, Float64, π, RoundUp),
+        (true, :stability_and_allocs, nothing, Float32, π, RoundDown),
+        (true, :stability_and_allocs, nothing, Float32, π, RoundUp),
+        (true, :stability_and_allocs, nothing, Float16, π, RoundDown),
+        (true, :stability_and_allocs, nothing, Float16, π, RoundUp),
     )
     memory = Any[_x, _dx]
     return test_cases, memory
@@ -197,10 +211,10 @@ function generate_derived_rrule!!_test_cases(
     test_cases = vcat(
         Any[
             # Tests for Base.CoreLogging @logmsg macros
-            (false, :stability, nothing, testloggingmacro1, rand(1:100)),
-            (false, :stability, nothing, testloggingmacro2, rand(1:100)),
-            (false, :stability, nothing, testloggingmacro3, rand(1:100)),
-            (false, :stability, nothing, testloggingmacro4, rand(1:100)),
+            (false, :none, nothing, testloggingmacro1, rand(1:100)),
+            (false, :none, nothing, testloggingmacro2, rand(1:100)),
+            (false, :none, nothing, testloggingmacro3, rand(1:100)),
+            (false, :none, nothing, testloggingmacro4, rand(1:100)),
         ],
     )
     return test_cases, Any[]
