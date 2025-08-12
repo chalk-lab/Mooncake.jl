be0e9dd997494bbc9ead5039d134ba085de575f2
Hong Ge <hg344@cam.ac.uk>
2025-08-12 20:10:21 +0100

    typofix

--- src/rrules/iddict.jl
+++ src/rrules/iddict.jl
@@ -214,7 +214,7 @@ end
 for name in
     [:(:jl_idtable_rehash), :(:jl_eqtable_put), :(:jl_eqtable_get), :(:jl_eqtable_nextind)]
     @eval function frule!!(::Dual{typeof(_foreigncall_)}, ::Dual{Val{$name}}, args...)
-        return unexepcted_foreigncall_error($name)
+        return unexpected_foreigncall_error($name)
     end
     @eval function rrule!!(::CoDual{typeof(_foreigncall_)}, ::CoDual{Val{$name}}, args...)
         return unexpected_foreigncall_error($name)
