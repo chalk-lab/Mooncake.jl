77240c02791e224d43568d39404e95e52abc651a
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-08-15 20:15:37 +0530

    include show in derived

--- src/rrules/avoiding_non_differentiable_code.jl
+++ src/rrules/avoiding_non_differentiable_code.jl
@@ -138,7 +138,6 @@ function generate_hand_written_rrule!!_test_cases(
             ("hello" => "hi",),
             1,
         ),
-        (false, :none, nothing, show, "Testing show"),
 
         # non-kwargs sprint rule test
         (false, :stability, nothing, sprint, show, "Testing sprint"),
@@ -200,8 +199,9 @@ function generate_derived_rrule!!_test_cases(
     test_cases = vcat(
         Any[
             # Tests for Base.CoreLogging, @show macros and string related functions.
-            (false, :none, nothing, (x) -> print(), 1),
-            (false, :none, nothing, (x) -> println(), 1),
+            (false, :none, nothing, (x) -> print(x), "Testing print"),
+            (false, :none, nothing, (x) -> println(x), "Testing println"),
+            (false, :none, nothing, (x) -> show(x), "Testing show"),
             (false, :none, nothing, testloggingmacro1, rand(1:100)),
             (false, :none, nothing, testloggingmacro2, rand(1:100)),
             (false, :none, nothing, testloggingmacro3, rand(1:100)),
