96bf6d29f17285151ded81fd6c191b0ab59f4fe4
Hong Ge <hg344@cam.ac.uk>
2025-08-28 19:47:15 +0100

    Revert to direct rrule!! approach for kron function performance optimization
    Co-authored-by: yebai <3279477+yebai@users.noreply.github.com>

--- src/rrules/performance_patches.jl
+++ src/rrules/performance_patches.jl
@@ -47,28 +47,213 @@ function rrule!!(
 end
 
 # Performance issue: https://github.com/chalk-lab/Mooncake.jl/issues/526
-# Use overlay implementations to provide efficient alternatives that Mooncake can differentiate
 
-@mooncake_overlay function kron(mat1::Matrix{T}, mat2::Matrix{T}) where {T<:IEEEFloat}
-    m1, n1 = size(mat1)
-    mat1_rsh = reshape(mat1, (1, m1, 1, n1))
+# Matrix × Matrix kron
+@is_primitive(DefaultCtx, Tuple{typeof(kron),Matrix{<:IEEEFloat},Matrix{<:IEEEFloat}})
+function frule!!(
+    ::Dual{typeof(kron)}, A::Dual{<:Matrix{P}}, B::Dual{<:Matrix{P}}
+) where {P<:IEEEFloat}
+    # Forward rule: d(kron(A, B)) = kron(dA, B) + kron(A, dB)
+    primal_A, tangent_A = primal(A), tangent(A)
+    primal_B, tangent_B = primal(B), tangent(B)
 
-    m2, n2 = size(mat2)
-    mat2_rsh = reshape(mat2, (m2, 1, n2, 1))
+    primal_result = kron(primal_A, primal_B)
+    tangent_result = kron(tangent_A, primal_B) + kron(primal_A, tangent_B)
 
-    return reshape(mat1_rsh .* mat2_rsh, (m1 * m2, n1 * n2))
+    return Dual(primal_result, tangent_result)
 end
 
-@mooncake_overlay function kron(a::Vector{T}, b::Vector{T}) where {T<:IEEEFloat}
-    return vec(kron(reshape(a, :, 1), reshape(b, :, 1)))
+function rrule!!(
+    ::CoDual{typeof(kron)}, A::CoDual{<:Matrix{P}}, B::CoDual{<:Matrix{P}}
+) where {P<:IEEEFloat}
+    primal_A, primal_B = A.x, B.x
+    dA, dB = A.dx, B.dx
+
+    function kron_pb!!(dy)
+        # Handle NoRData case
+        dy isa NoRData && return NoRData(), NoRData(), NoRData()
+        
+        m1, n1 = size(primal_A)
+        m2, n2 = size(primal_B)
+
+        # For dA: each element A[i,j] affects block dy[(i-1)*m2+1:i*m2, (j-1)*n2+1:j*n2] 
+        # The contribution is A[i,j] * B, so dA[i,j] += sum(dy[block] .* B)
+        for i in 1:m1, j in 1:n1
+            block_rows = ((i - 1) * m2 + 1):(i * m2)
+            block_cols = ((j - 1) * n2 + 1):(j * n2)
+            dA[i, j] += sum(view(dy, block_rows, block_cols) .* primal_B)
+        end
+
+        # For dB: each element B[k,l] appears in all blocks, multiplied by A[i,j]
+        # So dB[k,l] += sum over all i,j: A[i,j] * dy[block_i_j][k,l]
+        for k in 1:m2, l in 1:n2
+            for i in 1:m1, j in 1:n1
+                block_row = (i-1)*m2 + k
+                block_col = (j-1)*n2 + l
+                dB[k, l] += primal_A[i, j] * dy[block_row, block_col]
+            end
+        end
+
+        return NoRData(), NoRData(), NoRData()
+    end
+
+    return zero_fcodual(kron(primal_A, primal_B)), kron_pb!!
 end
 
-@mooncake_overlay function kron(a::Vector{T}, b::Matrix{T}) where {T<:IEEEFloat}
-    return kron(reshape(a, :, 1), b)
+# Vector × Vector kron
+@is_primitive(DefaultCtx, Tuple{typeof(kron),Vector{<:IEEEFloat},Vector{<:IEEEFloat}})
+function frule!!(
+    ::Dual{typeof(kron)}, a::Dual{<:Vector{P}}, b::Dual{<:Vector{P}}
+) where {P<:IEEEFloat}
+    # Convert to matrices and use matrix kron, then vectorize
+    primal_a, tangent_a = primal(a), tangent(a)
+    primal_b, tangent_b = primal(b), tangent(b)
+    
+    # kron(a, b) = vec(kron(reshape(a, :, 1), reshape(b, :, 1)))
+    a_mat = reshape(primal_a, :, 1)
+    b_mat = reshape(primal_b, :, 1)
+    da_mat = reshape(tangent_a, :, 1)
+    db_mat = reshape(tangent_b, :, 1)
+    
+    primal_result = vec(kron(a_mat, b_mat))
+    tangent_result = vec(kron(da_mat, b_mat) + kron(a_mat, db_mat))
+    
+    return Dual(primal_result, tangent_result)
 end
 
-@mooncake_overlay function kron(a::Matrix{T}, b::Vector{T}) where {T<:IEEEFloat}
-    return kron(a, reshape(b, :, 1))
+function rrule!!(
+    ::CoDual{typeof(kron)}, a::CoDual{<:Vector{P}}, b::CoDual{<:Vector{P}}
+) where {P<:IEEEFloat}
+    primal_a, primal_b = a.x, b.x
+    da, db = a.dx, b.dx
+
+    function kron_vec_pb!!(dy)
+        # Handle NoRData case
+        dy isa NoRData && return NoRData(), NoRData(), NoRData()
+        
+        # Convert vectors to column matrices
+        m1, m2 = length(primal_a), length(primal_b)
+        # dy is a vector of length m1*m2, reshape to matrix form
+        dy_mat = reshape(dy, m1 * m2, 1)
+        
+        # For da: each element a[i] contributes to dy[(i-1)*m2+1:i*m2]
+        for i in 1:m1
+            block_rows = ((i - 1) * m2 + 1):(i * m2)
+            da[i] += sum(dy_mat[block_rows, 1] .* primal_b)
+        end
+        
+        # For db: each element b[j] appears at positions j, m2+j, 2*m2+j, etc.
+        for j in 1:m2
+            indices = j:m2:(m1*m2)
+            db[j] += sum(primal_a[i] * dy[indices[i]] for i in 1:m1)
+        end
+
+        return NoRData(), NoRData(), NoRData()
+    end
+
+    return zero_fcodual(vec(kron(reshape(primal_a, :, 1), reshape(primal_b, :, 1)))), kron_vec_pb!!
+end
+
+# Vector × Matrix kron
+@is_primitive(DefaultCtx, Tuple{typeof(kron),Vector{<:IEEEFloat},Matrix{<:IEEEFloat}})
+function frule!!(
+    ::Dual{typeof(kron)}, a::Dual{<:Vector{P}}, B::Dual{<:Matrix{P}}
+) where {P<:IEEEFloat}
+    # kron(a, B) = kron(reshape(a, :, 1), B)
+    primal_a, tangent_a = primal(a), tangent(a)
+    primal_B, tangent_B = primal(B), tangent(B)
+    
+    a_mat = reshape(primal_a, :, 1)
+    da_mat = reshape(tangent_a, :, 1)
+    
+    primal_result = kron(a_mat, primal_B)
+    tangent_result = kron(da_mat, primal_B) + kron(a_mat, tangent_B)
+    
+    return Dual(primal_result, tangent_result)
+end
+
+function rrule!!(
+    ::CoDual{typeof(kron)}, a::CoDual{<:Vector{P}}, B::CoDual{<:Matrix{P}}
+) where {P<:IEEEFloat}
+    primal_a, primal_B = a.x, B.x
+    da, dB = a.dx, B.dx
+
+    function kron_vec_mat_pb!!(dy)
+        # Handle NoRData case
+        dy isa NoRData && return NoRData(), NoRData(), NoRData()
+        
+        m1 = length(primal_a)
+        m2, n2 = size(primal_B)
+        
+        # For da: each element a[i] affects block dy[(i-1)*m2+1:i*m2, :]
+        for i in 1:m1
+            block_rows = ((i - 1) * m2 + 1):(i * m2)
+            da[i] += sum(view(dy, block_rows, :) .* primal_B)
+        end
+        
+        # For dB: each element B[k,l] appears in all blocks, multiplied by a[i]
+        for k in 1:m2, l in 1:n2
+            for i in 1:m1
+                block_row = (i-1)*m2 + k
+                dB[k, l] += primal_a[i] * dy[block_row, l]
+            end
+        end
+
+        return NoRData(), NoRData(), NoRData()
+    end
+
+    return zero_fcodual(kron(reshape(primal_a, :, 1), primal_B)), kron_vec_mat_pb!!
+end
+
+# Matrix × Vector kron  
+@is_primitive(DefaultCtx, Tuple{typeof(kron),Matrix{<:IEEEFloat},Vector{<:IEEEFloat}})
+function frule!!(
+    ::Dual{typeof(kron)}, A::Dual{<:Matrix{P}}, b::Dual{<:Vector{P}}
+) where {P<:IEEEFloat}
+    # kron(A, b) = kron(A, reshape(b, :, 1))
+    primal_A, tangent_A = primal(A), tangent(A)
+    primal_b, tangent_b = primal(b), tangent(b)
+    
+    b_mat = reshape(primal_b, :, 1)
+    db_mat = reshape(tangent_b, :, 1)
+    
+    primal_result = kron(primal_A, b_mat)
+    tangent_result = kron(tangent_A, b_mat) + kron(primal_A, db_mat)
+    
+    return Dual(primal_result, tangent_result)
+end
+
+function rrule!!(
+    ::CoDual{typeof(kron)}, A::CoDual{<:Matrix{P}}, b::CoDual{<:Vector{P}}
+) where {P<:IEEEFloat}
+    primal_A, primal_b = A.x, b.x
+    dA, db = A.dx, b.dx
+
+    function kron_mat_vec_pb!!(dy)
+        # Handle NoRData case
+        dy isa NoRData && return NoRData(), NoRData(), NoRData()
+        
+        m1, n1 = size(primal_A)
+        m2 = length(primal_b)
+        
+        # For dA: each element A[i,j] affects block dy[(i-1)*m2+1:i*m2, j]
+        for i in 1:m1, j in 1:n1
+            block_rows = ((i - 1) * m2 + 1):(i * m2)
+            dA[i, j] += sum(view(dy, block_rows, j) .* primal_b)
+        end
+        
+        # For db: each element b[k] appears in all blocks, multiplied by A[i,j]
+        for k in 1:m2
+            for i in 1:m1, j in 1:n1
+                block_row = (i-1)*m2 + k
+                db[k] += primal_A[i, j] * dy[block_row, j]
+            end
+        end
+
+        return NoRData(), NoRData(), NoRData()
+    end
+
+    return zero_fcodual(kron(primal_A, reshape(primal_b, :, 1))), kron_mat_vec_pb!!
 end
 
 function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:performance_patches})
