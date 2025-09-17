f6444944dfb7ed845a82b285dc2cb1b587628404
Will Tebbutt <3628294+willtebbutt@users.noreply.github.com>
2025-09-17 08:24:49 +0100

    Improve comment

--- test/integration_testing/format/format.jl
+++ test/integration_testing/format/format.jl
@@ -1,6 +1,8 @@
-# Formatting is included in integration tests due to some dependency clashes with other
+# Formatting is outside the standard test env due to some dependency clashes with other
 # test dependencies. Putting it in its own environment ensures that there is no possibility
 # of such dependency clashes causing development issues in the future.
+#
+# Locating this amongst the integration tests is purely a matter of convenience.
 
 using Pkg
 Pkg.activate(@__DIR__)
