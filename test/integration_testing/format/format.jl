e9330046c78ff493b07139d455d89f9a6ce927dd
Will Tebbutt <3628294+willtebbutt@users.noreply.github.com>
2025-09-17 08:21:12 +0100

    Comment on why formatting happens in integration tests

--- test/integration_testing/format/format.jl
+++ test/integration_testing/format/format.jl
@@ -1,3 +1,7 @@
+# Formatting is included in integration tests due to some dependency clashes with other
+# test dependencies. Putting it in its own environment ensures that there is no possibility
+# of such dependency clashes causing development issues in the future.
+
 using Pkg
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
