ac915b955264e6b5aece7ea8be767d1db03f8d89
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-10-04 00:24:48 +0530

    tests

--- test/rrules/builtins.jl
+++ test/rrules/builtins.jl
@@ -1,6 +1,17 @@
 foo_throws(e) = throw(e)
 
 @testset "builtins" begin
+    # NaN handling in rrules
+    test_cases = vcat(map([Float16, Float32, Float64]) do T
+        cases = [(Base.sqrt_llvm, T(0)), (Base.sqrt_llvm_fast, T(0))]
+        return cases
+    end...)
+    for (f, args) in test_cases
+        cache = prepare_gradient_cache(f, args...)
+        _, grad = value_and_gradient!!(cache, f, args...)
+        @test all(iszero, grad[2:end])
+    end
+
     @test_throws(
         ErrorException,
         Mooncake.rrule!!(CoDual(IntrinsicsWrappers.add_ptr, NoTangent()), 5.0, 4.0),
