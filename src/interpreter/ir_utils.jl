ea7c4daae0bfa2ca2625f415931160c61294b844
serenity4 <cedric.bel@hotmail.fr>
2025-10-22 14:36:01 +0000

    Make version check static

--- src/interpreter/ir_utils.jl
+++ src/interpreter/ir_utils.jl
@@ -83,11 +83,11 @@ linetable field has at least one element.
 """
 function __insts_to_instruction_stream(insts::Vector{Any})
     n = length(insts)
-    if VERSION > v"1-12-"
+    @static if VERSION > v"1-12-"
         lineinfo = Int32[]
         for _ in 1:n push!(lineinfo, 1, 0, 0) end
     else
-        lineinfo = fill(Int32(1), n)
+        lineinfo = ones(Int32, n)
     end
     return CC.InstructionStream(
         insts,
