6c11f4322bec25533e9e53804ccad47096df60f0
Markus Hauru <mhauru@turing.ac.uk>
2025-10-23 17:45:23 +0100

    Make Base.CoreLogging.handle_message_nothrow as having @zero_derivative
    This comes up in logmsg_code, because in Julia v1.12
    handle_message_nothrow got a try-catch structure, which creates an
    UpsilonNode that Mooncake can't handle.

--- src/rrules/avoiding_non_differentiable_code.jl
+++ src/rrules/avoiding_non_differentiable_code.jl
@@ -70,6 +70,8 @@ import Base.CoreLogging as CoreLogging
     Int64,
 }
 @zero_derivative MinimalCtx Tuple{typeof(Base.fixup_stdlib_path), String}
+@zero_derivative MinimalCtx Tuple{typeof(Core.kwcall),NamedTuple,typeof(Base.CoreLogging.handle_message_nothrow),Vararg}
+
 # specialized case for Builtin primitive Core._call_latest rrule for CoreLogging.handle_message kwargs call.
 @zero_derivative(
     MinimalCtx,
