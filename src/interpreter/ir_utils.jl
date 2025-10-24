864e80cbda9629184aa724001a797603659c9a56
Markus Hauru <mhauru@turing.ac.uk>
2025-10-24 11:14:13 +0100

    Improve a comment

--- src/interpreter/ir_utils.jl
+++ src/interpreter/ir_utils.jl
@@ -130,8 +130,8 @@ end
 function __infer_ir!(ir, interp::CC.AbstractInterpreter, mi::CC.MethodInstance)
     @static if VERSION >= v"1.12-"
         nargs = length(ir.argtypes) - 1
-        # TODO(mhauru) How do we figure out isva? I don't think it's in ir or mi, see above
-        # prints.
+        # TODO(mhauru) How do we figure out isva? I don't think it's in ir or mi. Maybe it
+        # doesn't matter given the way we use SpecInfo here?
         isva = false
         propagate_inbounds = true
         spec_info = CC.SpecInfo(nargs, isva, propagate_inbounds, nothing)
