cf4131c0f061f3df82bd2939c5ed6a36d8e04f0b
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-11-10 15:59:14 +0000

    Skip certain stability and performance tests for 1.12 (#844)
    * Refactor stability rules for Float types in Julia
    
    Updated stability and allocation rules for Float types based on Julia version.
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Remove Float16 cases from test cases
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Update avoiding_non_differentiable_code.jl
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * reformat
    
    ---------
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- src/rrules/avoiding_non_differentiable_code.jl
+++ src/rrules/avoiding_non_differentiable_code.jl
@@ -107,6 +107,13 @@ end
     }
 )
 
+# Julia Base functions have type stability issues in version 1.12
+const PERF_FLAG = @static if VERSION >= v"1.12-"
+    :none
+else
+    :stability_and_allocs
+end
+
 function hand_written_rule_test_cases(rng_ctor, ::Val{:avoiding_non_differentiable_code})
     _x = Ref(5.0)
     _dx = Ref(4.0)
@@ -162,12 +169,14 @@ function hand_written_rule_test_cases(rng_ctor, ::Val{:avoiding_non_differentiab
         # Rules to make Symbol-related functionality work properly.
         (false, :stability_and_allocs, nothing, Symbol, "hello"),
         (false, :stability_and_allocs, nothing, Symbol, UInt8[1, 2]),
-        (false, :stability_and_allocs, nothing, Float64, π, RoundDown),
-        (false, :stability_and_allocs, nothing, Float64, π, RoundUp),
-        (true, :stability_and_allocs, nothing, Float32, π, RoundDown),
-        (true, :stability_and_allocs, nothing, Float32, π, RoundUp),
-        (true, :stability_and_allocs, nothing, Float16, π, RoundDown),
-        (true, :stability_and_allocs, nothing, Float16, π, RoundUp),
+
+        # Julia Base functions have type stability issues in version 1.12
+        (false, PERF_FLAG, nothing, Float64, π, RoundDown),
+        (false, PERF_FLAG, nothing, Float64, π, RoundUp),
+        (true, PERF_FLAG, nothing, Float32, π, RoundDown),
+        (true, PERF_FLAG, nothing, Float32, π, RoundUp),
+        (true, PERF_FLAG, nothing, Float16, π, RoundDown),
+        (true, PERF_FLAG, nothing, Float16, π, RoundUp),
     )
     memory = Any[_x, _dx]
     return test_cases, memory
