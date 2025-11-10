b0b69e80bf25fffde8dd954e1acaa79281730f4f
Xianda Sun <sunxdt@gmail.com>
2025-11-10 12:04:28 +0000

    add  forward over reverse test

--- test/ext/differentiation_interface/differentiation_interface.jl
+++ test/ext/differentiation_interface/differentiation_interface.jl
@@ -11,16 +11,3 @@ test_differentiation(
     excluded=SECOND_ORDER,
     logging=true,
 )
-
-# Explicit second-order sanity tests for Mooncake forward-over-reverse
-@testset "Mooncake second-order examples" begin
-    backend = SecondOrder(AutoMooncakeForward(), AutoMooncake())
-
-    # Sum: Hessian is zero
-    @test DI.hessian(sum, backend, [2.0]) == [0.0]
-
-    # Rosenbrock 2D at [1.2, 1.2]
-    rosen(z) = (1.0 - z[1])^2 + 100.0 * (z[2] - z[1]^2)^2
-    H = DI.hessian(rosen, backend, [1.2, 1.2])
-    @test isapprox(H, [1250.0 -480.0; -480.0 200.0]; rtol=1e-10, atol=1e-12)
-end
