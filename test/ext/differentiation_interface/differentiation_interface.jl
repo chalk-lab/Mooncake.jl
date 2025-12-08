14b6fcf7336ac01f46cdb5ca8cc5b3d3e03d0e9b
Xianda Sun <sunxdt@gmail.com>
2025-12-08 12:28:31 +0000

    test forward over forward to see

--- test/ext/differentiation_interface/differentiation_interface.jl
+++ test/ext/differentiation_interface/differentiation_interface.jl
@@ -14,8 +14,13 @@ test_differentiation(
 
 # Test second-order differentiation (forward-over-reverse for Hessian)
 test_differentiation(
-    [SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing))],
-    DifferentiationInterfaceTest.default_scenarios(; linalg=false);
+    [
+        SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing)),
+        SecondOrder(
+            AutoMooncakeForward(; config=nothing), AutoMooncakeForward(; config=nothing)
+        ),
+    ],
+    # DifferentiationInterfaceTest.default_scenarios(; linalg=false); # no_linalg uses loop, not broadcast
     excluded=vcat(FIRST_ORDER, [:hvp, :second_derivative]),
     logging=true,
 )
