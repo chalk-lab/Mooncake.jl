b7af9bbc0539042dc86136ebfa16f06550ae626e
Penelope Yong <penelopeysm@gmail.com>
2025-12-08 17:30:13 +0000

    Implement `logsumexp` rule

--- ext/MooncakeLogExpFunctionsExt.jl
+++ ext/MooncakeLogExpFunctionsExt.jl
@@ -1,9 +1,21 @@
 module MooncakeLogExpFunctionsExt
 
-using LogExpFunctions, Mooncake
+using LinearAlgebra: dot
+using LogExpFunctions
 using Base: IEEEFloat
-
-import Mooncake: DefaultCtx, @from_chainrules
+import Mooncake:
+    DefaultCtx,
+    @from_chainrules,
+    frule!!,
+    rrule!!,
+    Dual,
+    CoDual,
+    primal,
+    tangent,
+    @is_primitive,
+    zero_fcodual,
+    NoRData,
+    extract
 
 @from_chainrules DefaultCtx Tuple{typeof(xlogx),IEEEFloat}
 @from_chainrules DefaultCtx Tuple{typeof(xlogy),IEEEFloat,IEEEFloat}
@@ -25,5 +37,76 @@ import Mooncake: DefaultCtx, @from_chainrules
 @from_chainrules DefaultCtx Tuple{typeof(logsubexp),IEEEFloat,IEEEFloat}
 @from_chainrules DefaultCtx Tuple{typeof(cloglog),IEEEFloat}
 @from_chainrules DefaultCtx Tuple{typeof(cexpexp),IEEEFloat}
+
+# logsumexp needs a custom rule to avoid incorrect derivatives due to branching in the
+# primal implementation. In principle, the forward-mode rules could be imported from
+# ChainRulesCore but that leads to extra allocations, so we reimplement them.
+@is_primitive DefaultCtx Tuple{typeof(logsumexp),AbstractArray{<:IEEEFloat}}
+@is_primitive DefaultCtx Tuple{
+    typeof(Core.kwcall),NamedTuple,typeof(logsumexp),AbstractArray{<:IEEEFloat}
+}
+function frule!!(
+    ::Dual{typeof(Core.kwcall)},
+    kwargs::Dual{<:NamedTuple},
+    ::Dual{typeof(logsumexp)},
+    x::Dual{<:AbstractArray{P}},
+) where {P<:IEEEFloat}
+    y = logsumexp(primal(x); primal(kwargs)...)
+    dy = sum(tangent(x) .* (exp.(primal(x) .- y)); primal(kwargs)...)
+    return Dual(y, dy)
+end
+function frule!!(
+    ::Dual{typeof(logsumexp)}, x::Dual{<:AbstractArray{P}}
+) where {P<:IEEEFloat}
+    y = logsumexp(primal(x))
+    dy = zero(P)
+    xp, dx = extract(x)
+    # same as dy = dot(dx, exp.(xp .- y)) but unrolled to avoid allocations
+    for i in eachindex(dx)
+        @inbounds dy += dx[i] * exp(xp[i] - y)
+    end
+    return Dual(y, dy)
+end
+function rrule!!(
+    ::CoDual{typeof(Core.kwcall)},
+    kwargs::CoDual{<:NamedTuple{(:dims,),<:Tuple{Colon}}},
+    ::CoDual{typeof(logsumexp)},
+    x::CoDual{<:AbstractArray{P}},
+) where {P<:IEEEFloat}
+    y = logsumexp(primal(x); primal(kwargs)...)
+    dx = tangent(x)
+    function logsumexp_pb!!(dy::P)
+        dx .+= dy * exp.(primal(x) .- y)
+        return NoRData(), NoRData(), NoRData(), NoRData()
+    end
+    return zero_fcodual(y), logsumexp_pb!!
+end
+function rrule!!(
+    ::CoDual{typeof(Core.kwcall)},
+    # all other dim arguments - i.e. ints or tuples thereof
+    kwargs::CoDual{<:NamedTuple{(:dims,),<:Tuple{Any}}},
+    ::CoDual{typeof(logsumexp)},
+    x::CoDual{<:AbstractArray{P}},
+) where {P<:IEEEFloat}
+    y = logsumexp(primal(x); primal(kwargs)...)
+    dy = zero(y)
+    dx = tangent(x)
+    function logsumexp_pb!!(::NoRData)
+        dx .+= dy .* exp.(primal(x) .- y)
+        return NoRData(), NoRData(), NoRData(), NoRData()
+    end
+    return CoDual(y, dy), logsumexp_pb!!
+end
+function rrule!!(
+    ::CoDual{typeof(logsumexp)}, x::CoDual{<:AbstractArray{P}}
+) where {P<:IEEEFloat}
+    y = logsumexp(primal(x))
+    dx = tangent(x)
+    function logsumexp_pb!!(dy::P)
+        dx .+= dy .* exp.(primal(x) .- y)
+        return NoRData(), NoRData()
+    end
+    return zero_fcodual(y), logsumexp_pb!!
+end
 
 end
