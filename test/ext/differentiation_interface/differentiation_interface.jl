5db1ce232aa8af71a369c52a7983fdf434253d5e
Xianda Sun <sunxdt@gmail.com>
2025-12-08 17:47:43 +0000

    remove added DI tests

--- test/ext/differentiation_interface/differentiation_interface.jl
+++ test/ext/differentiation_interface/differentiation_interface.jl
@@ -11,16 +11,3 @@ test_differentiation(
     excluded=SECOND_ORDER,
     logging=true,
 )
-
-# Test second-order differentiation (forward-over-reverse for Hessian)
-test_differentiation(
-    [
-        SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing)),
-        SecondOrder(
-            AutoMooncakeForward(; config=nothing), AutoMooncakeForward(; config=nothing)
-        ),
-    ],
-    # DifferentiationInterfaceTest.default_scenarios(; linalg=false); # no_linalg uses loop, not broadcast
-    excluded=vcat(FIRST_ORDER, [:hvp, :second_derivative]),
-    logging=true,
-)
