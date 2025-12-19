00837c8b83a31dab3a836195b80d67de2369889e
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-12-19 17:07:01 +0000

    format fr

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -774,7 +774,7 @@ function test_frule_performance(
             ),
         )
     end
-    
+
     performance_checks_flag == :none && return nothing
 
     if performance_checks_flag in (:stability, :stability_and_allocs)
@@ -1032,6 +1032,7 @@ function run_hand_written_rule_test_cases(rng_ctor, v::Val, mode::Type{<:Mode})
     GC.@preserve memory @testset "$f, $(_typeof(x))" for (
         interface_only, perf_flag, _, f, x...
     ) in test_cases
+
         test_rule(rng_ctor(123), f, x...; interface_only, perf_flag, mode)
     end
 end
@@ -1043,6 +1044,7 @@ function run_derived_rule_test_cases(rng_ctor, v::Val, mode::Type{<:Mode})
     GC.@preserve memory @testset "$mode, $f, $(typeof(x))" for (
         interface_only, perf_flag, _, f, x...
     ) in test_cases
+
         test_rule(
             rng_ctor(123), f, x...; interface_only, perf_flag, is_primitive=false, mode
         )
