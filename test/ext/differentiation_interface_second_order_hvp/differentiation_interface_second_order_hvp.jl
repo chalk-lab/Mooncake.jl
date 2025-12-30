13eece5709473a43c0e6c091cdf6ea27cfc8886a
Hong Ge <hg344@cam.ac.uk>
2025-12-30 23:31:27 +0000

    use simple prep in DI

--- test/ext/differentiation_interface_second_order_hvp/differentiation_interface_second_order_hvp.jl
+++ test/ext/differentiation_interface_second_order_hvp/differentiation_interface_second_order_hvp.jl
@@ -5,6 +5,10 @@ Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 using DifferentiationInterface, DifferentiationInterfaceTest
 using Mooncake: Mooncake
 
+function DifferentiationInterface.inner_preparation_behavior(::AutoMooncakeForward)
+    DifferentiationInterface.PrepareInnerSimple()
+end
+
 # Test second-order differentiation (forward-over-reverse)
 test_differentiation(
     [SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing))];
