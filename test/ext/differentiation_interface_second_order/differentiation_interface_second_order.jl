e42aec5ed05712024a7662b018f34cb0cc362ce7
Hong Ge <3279477+yebai@users.noreply.github.com>
2026-01-06 21:59:10 +0000

    Update exclusion criteria for second-order tests
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- test/ext/differentiation_interface_second_order/differentiation_interface_second_order.jl
+++ test/ext/differentiation_interface_second_order/differentiation_interface_second_order.jl
@@ -5,9 +5,18 @@ Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 using DifferentiationInterface, DifferentiationInterfaceTest
 using Mooncake: Mooncake
 
+EXCLUDED = @static if VERSION ≥ v"1.11-" && VERSION ≤ v"1.12-"
+    # testing only :hessian on 1.11 due to opaque closure bug.  
+    # This is potentially the same issue as discussed in 
+    # https://github.com/chalk-lab/MistyClosures.jl/pull/12#issue-3278662295
+    [FIRST_ORDER..., :hvp, :second_derivative],
+else
+    [FIRST_ORDER...],
+end
+
 # Test second-order differentiation (forward-over-reverse)
 test_differentiation(
     [SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing))];
-    excluded=[FIRST_ORDER..., :hvp, :second_derivative], # testing only :hessian
+    excluded=EXCLUDED
     logging=true,
 )
