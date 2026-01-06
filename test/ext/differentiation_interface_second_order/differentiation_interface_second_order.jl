62b3c95dc2509cf82e6f97b08eafe11fe4ae011e
Hong Ge <3279477+yebai@users.noreply.github.com>
2026-01-06 22:14:16 +0000

    Fix syntax for excluded parameter in test_differentiation
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- test/ext/differentiation_interface_second_order/differentiation_interface_second_order.jl
+++ test/ext/differentiation_interface_second_order/differentiation_interface_second_order.jl
@@ -9,14 +9,14 @@ EXCLUDED = @static if VERSION ≥ v"1.11-" && VERSION ≤ v"1.12-"
     # testing only :hessian on 1.11 due to opaque closure bug.  
     # This is potentially the same issue as discussed in 
     # https://github.com/chalk-lab/MistyClosures.jl/pull/12#issue-3278662295
-    [FIRST_ORDER..., :hvp, :second_derivative],
+    [FIRST_ORDER..., :hvp, :second_derivative]
 else
-    [FIRST_ORDER...],
+    [FIRST_ORDER...]
 end
 
 # Test second-order differentiation (forward-over-reverse)
 test_differentiation(
     [SecondOrder(AutoMooncakeForward(; config=nothing), AutoMooncake(; config=nothing))];
-    excluded=EXCLUDED
+    excluded=EXCLUDED,
     logging=true,
 )
