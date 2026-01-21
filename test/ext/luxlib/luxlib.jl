537308cc6fed23c40d16bd3757f5f39945657263
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2026-01-21 18:11:39 +0000

    begin

--- test/ext/luxlib/luxlib.jl
+++ test/ext/luxlib/luxlib.jl
@@ -89,3 +89,36 @@ using Mooncake.TestUtils: test_rule
         test_rule(StableRNG(123), fargs...; perf_flag, is_primitive, interface_only, mode)
     end
 end
+
+
+# tests pass but only fro derived rule.
+
+# Lux kernel fusion
+
+using Mooncake, LuxLib, NNlib, Zygote, Random
+using Test
+# @testset for wT in (Float32, Float64),
+wT = Float32
+xT = Float32
+# , Float64)
+has_bias = true
+#  false)
+
+x = randn(xT, 3, 6)
+weight = randn(wT, 2, 3)
+bias = has_bias ? randn(wT, 2) : nothing
+
+fn = sum ∘ fused_dense_bias_activation
+
+cache = Mooncake.build_rrule(fn, gelu, weight, x, bias)
+_, (_, _, ∂weight, ∂x, ∂bias) = value_and_gradient!!(cache, fn, gelu, weight, x, bias)
+
+_, ∂weight_zyg, ∂x_zyg, ∂bias_zyg = Zygote.gradient(fn, gelu, weight, x, bias)
+
+@test ∂x ≈ ∂x_zyg atol = 1.0e-3 rtol = 1.0e-3
+@test ∂weight ≈ ∂weight_zyg atol = 1.0e-3 rtol = 1.0e-3
+if has_bias
+    @test ∂bias ≈ ∂bias_zyg atol = 1.0e-3 rtol = 1.0e-3
+end
+# end
+
