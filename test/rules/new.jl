f4b89c9fbee886c9f586fdfb4158ce8bb2e8d975
Hong Ge <hg344@cam.ac.uk>
2026-02-26 21:24:14 +0000

    merge test files to reduce duplication

--- test/rules/new.jl
+++ test/rules/new.jl
@@ -1,5 +1,4 @@
 @testset "new" begin
     TestUtils.run_rule_test_cases(StableRNG, Val(:new))
-    include("build_fdata_world_age_regression.jl")
-    include("build_output_tangent_world_age_regression.jl")
+    include("tangent_world_age_regression.jl")
 end
