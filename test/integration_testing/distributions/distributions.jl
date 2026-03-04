40735819c829aa3f423efdbe19fef1d02c654ca6
Hong Ge <3279477+yebai@users.noreply.github.com>
2026-03-04 13:11:05 +0000

    Uncomment NormalInverseGaussian test case
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- test/integration_testing/distributions/distributions.jl
+++ test/integration_testing/distributions/distributions.jl
@@ -111,7 +111,7 @@ sr(n::Int) = StableRNG(n)
         (:allocs, Normal(0.5, 1.0), 0.05),
         (:allocs, Normal(0.0, 1.5), -0.1),
         (:allocs, Normal(-0.1, 0.9), -0.3),
-        # (:none NormalInverseGaussian(0.0, 1.0, 0.2, 0.1), 0.1), # foreigncall -- https://github.com/JuliaMath/SpecialFunctions.jl/blob/be1fa06fee58ec019a28fb0cd2b847ca83a5af9a/src/bessel.jl#L265
+        (:none NormalInverseGaussian(0.0, 1.0, 0.2, 0.1), 0.1),
         (:allocs, Pareto(1.0, 1.0), 3.5),
         (:allocs, Pareto(1.1, 0.9), 3.1),
         (:allocs, Pareto(1.0, 1.0), 1.4),
