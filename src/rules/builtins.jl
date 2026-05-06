6b066eda2feacafc521c1889aac620c0c80df29e
Hong Ge <hg344@cam.ac.uk>
2026-05-06 21:52:26 +0100

    Relax svec performance bounds

--- src/rules/builtins.jl
+++ src/rules/builtins.jl
@@ -1402,11 +1402,11 @@ function hand_written_rule_test_cases(rng_ctor, ::Val{:builtins})
         (false, :none, _range, Core._svec_ref, svec(5, 4), 2),
         (false, :none, _range, Core._svec_ref, svec(5, 4.0), 2),
         (false, :none, _range, Core._svec_ref, svec(5, randn(rng_ctor(1234), 2, 3)), 2),
-        (false, :none, _range, Core.svec, 5, 4.0, randn(rng_ctor(1234), 2, 3)),
+        (false, :none, (lb=1e-3, ub=500.0), Core.svec, 5, 4.0, randn(rng_ctor(1234), 2, 3)),
         # check svec with no arguments
         (false, :none, _range, Core.svec),
         # check svec with an argument that has both fdata and rdata
-        (false, :none, _range, Core.svec, (5, 4.0, randn(rng_ctor(1234), 2, 3))),
+        (false, :none, (lb=1e-3, ub=500.0), Core.svec, (5, 4.0, randn(rng_ctor(1234), 2, 3))),
         # Core._typebody! -- NEEDS IMPLEMENTING AND TESTING
         (false, :stability, nothing, <:, Float64, Int),
         (false, :stability, nothing, <:, Any, Float64),
