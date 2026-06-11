e1e723767838228db3f58a414b4b961475bbb129
AstitvaAggarwal <astitvaaggarwalimportant@gmail.com>
2026-06-11 17:11:58 +0100

    kwarg rule version

--- src/rules/avoiding_non_differentiable_code.jl
+++ src/rules/avoiding_non_differentiable_code.jl
@@ -100,24 +100,27 @@ end
     }
 end
 
-# specialized case for Builtin primitive Core._call_latest rrule for CoreLogging.handle_message kwargs call.
-@zero_derivative(
-    MinimalCtx,
-    Tuple{
-        typeof(Core._call_latest),
-        typeof(Core.kwcall),
-        NamedTuple,
-        typeof(CoreLogging.handle_message),
-        Any,
-        Base.CoreLogging.LogLevel,
-        String,
-        Module,
-        Symbol,
-        Symbol,
-        String,
-        Int64,
-    }
-)
+# On 1.12+, @invokelatest no longer expands to Core._call_latest, so this kwargs
+# variant is also dead code on 1.12+.
+@static if VERSION < v"1.12-"
+    @zero_derivative(
+        MinimalCtx,
+        Tuple{
+            typeof(Core._call_latest),
+            typeof(Core.kwcall),
+            NamedTuple,
+            typeof(CoreLogging.handle_message),
+            Any,
+            Base.CoreLogging.LogLevel,
+            String,
+            Module,
+            Symbol,
+            Symbol,
+            String,
+            Int64,
+        }
+    )
+end
 
 function hand_written_rule_test_cases(rng_ctor, ::Val{:avoiding_non_differentiable_code})
     _x = Ref(5.0)
